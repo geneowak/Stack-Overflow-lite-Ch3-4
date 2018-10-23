3f292669f7246fdf80899595c748291ebfc41ecb
geneowak <geneowak@gmail.com>
2018-10-23 12:18:59 +0300

    add check to create database if it doesn't exist before the first request

--- API/database/comment_db_handler.py
+++ API/database/comment_db_handler.py
@@ -6,9 +6,11 @@ from .database_ini import table_names
 
 class CommentHandler(DbHandler):
     ''' 
-    This method handles all the database functions of the answer model
+    This method handles all the database functions of the comment model
     '''
-    # table cols
+
+    # question comment table cols: id, comment, qn_id, user_id, create_date
+    # answer comment table cols: id, comment, ans_id, user_id, create_date
     def __init__(self):
         super().__init__()
         self.qn_table_name = table_names["question_comments"]
@@ -29,41 +31,133 @@ class CommentHandler(DbHandler):
             super().close_conn()
             return False
 
-    def insert_ans_comment(self):
-        ''' adds an answer comment to the database '''
-        pass
-
-    def update_qn_comment(self):
+    def update_qn_comment(self, comment, qn_id):
         ''' updates a question comment in the database '''
-        pass
+        try:
+            query = "UPDATE "+self.ans_table_name + " SET comment=%s WHERE qn_id=%s"
+            self.cursor.execute(query, (comment, qn_id))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
 
-    def update_ans_comment(self):
-        ''' updates an answer comment in the database '''
-        pass
-
-    def get_qn_comment_by_qn_id(self):
+    def get_qn_comment_by_qn_id(self, qn_id):
         ''' returns the specified question comment from the database '''
-        pass
+        try:
+            query = "SELECT comment, id, user_id, qn_id FROM "+self.ans_table_name+" WHERE qn_id=%s"
+            self.cursor.execute(query, (qn_id,))
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
 
-    def get_ans_comment_by_ans_id(self):
-        ''' returns the specified answer comment from the database '''
-        pass
-
-    def delete_qn_comment_by_qn_id(self):
+    def delete_qn_comment_by_qn_id(self, comment_id):
         ''' deletes the specified question comment from the database '''
-        pass
+        try:
+            query = "DELETE FROM "+self.qn_table_name+" WHERE id=%s"
+            self.cursor.execute(query, (comment_id,))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
 
-    def delete_ans_comment_by_ans_id(self):
-        ''' deletes the specified answer comment from the database '''
-        pass
-
-    def check_repeated_ans_comment(self):
-        ''' checks if the specified answer comment already exists in the database '''
-        pass
-
-    def check_repeated_qn_comment(self):
+    def check_repeated_qn_comment(self, comment):
         ''' checks if the specified question comment already exists in the database '''
-        pass
+        try:
+            query = "SELECT comment, id, user_id, qn_id FROM " + \
+                self.qn_table_name+" WHERE comment=%s"
+            self.cursor.execute(query, (comment,))
+            row = self.cursor.fetchone()
+            super().close_conn()
+            return row
+        except (Exception) as error:
+            pprint(error)
+            super().close_conn()
+            return False
+
+    def insert_ans_comment(self, user_id, ans_id, comment):
+        ''' adds an answer comment to the database '''
+        try:
+            query = "INSERT INTO "+self.ans_table_name + \
+                " (comment, ans_id, user_id, create_date) VALUES(%s,%s,%s,%s)"
+            self.cursor.execute(
+                query, (comment, ans_id, user_id, datetime.datetime.now()))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
+
+    def update_ans_comment(self, user_id, ans_id, comment):
+        ''' updates an answer comment in the database '''
+        try:
+            query = "UPDATE "+self.ans_table_name + " SET comment=%s WHERE ans_id=%s"
+            self.cursor.execute(query, (comment, ans_id))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
+
+    def get_ans_comment_by_ans_id(self, user_id, ans_id, comment):
+        ''' returns the specified answer comment from the database '''
+        try:
+            query = "SELECT comment, id, user_id, ans_id FROM "+self.ans_table_name+" WHERE ans_id=%s"
+            self.cursor.execute(query, (ans_id,))
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
+
+    def delete_ans_comment_by_ans_id(self, comment_id):
+        ''' deletes the specified answer comment from the database '''
+        try:
+            query = "DELETE FROM "+self.ans_table_name+" WHERE id=%s"
+            self.cursor.execute(query, (comment_id,))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint(error)
+            self.conn.rollback()
+            super().close_conn()
+            return False
+
+    def check_repeated_ans_comment(self, comment):
+        ''' checks if the specified answer comment already exists in the database '''
+        try:
+            query = "SELECT comment, id, user_id, ans_id FROM " + \
+                self.ans_table_name+" WHERE comment=%s"
+            self.cursor.execute(query, (comment,))
+            row = self.cursor.fetchone()
+            super().close_conn()
+            return row
+        except (Exception) as error:
+            pprint(error)
+            super().close_conn()
+            return False
 
     
         
--- API/database/db_handler.py
+++ API/database/db_handler.py
@@ -17,9 +17,13 @@ class DbHandler:
                 self.conn = psycopg2.connect(os.getenv('DATABASE_URL'))
                 # pprint("Using HEROKU db....")
             elif app.config['TESTING']:
+                pprint("....creating database...")
+                self.create_database(test_db_config)
                 self.conn = psycopg2.connect(**test_db_config)
                 # pprint("Using test db....")
             else:
+                pprint("....creating database...")
+                self.create_database(development_db_config)
                 self.conn = psycopg2.connect(**development_db_config)
                 # pprint("Using development db....")
             self.conn.autocommit = True
@@ -45,6 +49,45 @@ class DbHandler:
         ''' deletes all the tables in the database '''
         for key in table_names:
             self.cursor.execute("DROP TABLE IF EXISTS {} CASCADE".format(table_names[key]))
+
+    def create_database(self, dbconfig):
+        ''' creates a database if it doesn't exist '''
+        print("....Starting db creation...", str(dbconfig))
+
+        try:
+            dbname = dbconfig["dbname"]
+            paswd = dbconfig["password"]
+            user = dbconfig["user"]
+            host = dbconfig["host"]
+
+            # make a connection
+            conn = psycopg2.connect(dbname="postgres", user=user, host=host, password=paswd)
+
+            conn.autocommit = True
+            # get a cursor
+            cursor = conn.cursor()            
+            
+            get_db_query = "SELECT datname FROM pg_catalog.pg_database WHERE datname = '{}';".format(dbname)
+
+            cursor.execute(get_db_query)
+            # get result
+            exists = cursor.fetchone()
+            pprint("....Printing result...")
+            pprint(exists)
+            if not exists:
+                cursor.execute("CREATE DATABASE {}".format(dbname))
+
+            # close the connection
+            cursor.close()
+            conn.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print("and error occured", error)
+            raise error
+            
+
+
+        
     
 
 # if __name__ == '__main__':
