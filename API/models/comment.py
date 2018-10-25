d954f7e77ed8675676b985ee0849af72f8baafb8
Eugene Owak <13153526+geneowak@users.noreply.github.com>
2018-10-25 15:59:46 +0300

    Revert "Add comments end points (#4)"
    This reverts commit 9ba8bade97db32be810b8658d0f91ccbfe0dd1b4.

--- API/models/comment.py
+++ API/models/comment.py
@@ -1,48 +1,54 @@
 from API.models.question import Question
 from API.models.answer import Answer
-from API.database.comment_db_handler import CommentsHandler
-from pprint import pprint
-
-
 class Comment:
 
     comments = []
-    # question comment table cols: id, comment, qn_id, user_id, create_date
-    # answer comment table cols: id, comment, ans_id, user_id, create_date
-    def __init__(self, user_id, comment, parent, parent_id):
-        self.user_id = user_id
-        self.comment = comment
+
+    def __init__(self, _id, body, parent, parent_id):
+        self.id = _id
+        self.body = body
         self.parent = parent # NUM 'question', 'comment'
         self.parent_id = parent_id
 
     def json(self):
         return {
-            "user_id": self.user_id,
-            "comment": self.comment,
+            "id": self.id,
+            "body": self.body,
             "parent": self.parent,
             "parent_id": self.parent_id
         }
 
-    def add_comment(self):
+    @classmethod
+    def add_comment(cls, comment):
         ''' this method first checks if a parent exists and if it does, it adds an comment to it
         True is returned for success and False when it fails to add the comment '''
-        pprint("adding comment...")
-        pprint(self.json())
-        if self.parent.lower() == 'question':
-            # print(self.json())
-            if Question.get_question_by_id(self.parent_id):
+        if comment.parent.lower() == 'question':
+            print(comment.json())
+            if Question.get_question_by_id(comment.parent_id):
+                comm = {
+                    "id": comment.id,
+                    "body": comment.body,
+                    "parent": comment.parent,
+                    "parent_id": comment.parent_id
+                }
+                cls.comments.append(comm)
                 try:
-                    handler = CommentsHandler()
-                    handler.insert_qn_comment(self.user_id, self.parent_id, self.comment)
+                    Question.add_comment(comment.parent_id, comm)
                 except:
                     return False
                 return True
             return False
-        elif self.parent.lower() == 'answer':
-            if Answer.get_answer_by_ans_id(self.parent_id):
+        elif comment.parent.lower() == 'answer':
+            if Answer.get_answer_by_id(comment.parent_id):
+                comm = {
+                    "id": comment.id,
+                    "body": comment.body,
+                    "parent": comment.parent,
+                    "parent_id": comment.parent_id
+                }
+                cls.comments.append(comm)
                 try:
-                    handler = CommentsHandler()
-                    handler.insert_ans_comment(self.user_id, self.parent_id, self.comment)
+                    Answer.add_comment(comment.parent_id, comm)
                 except:
                     return False
                 return True
@@ -51,76 +57,22 @@ class Comment:
             
     @classmethod
     def get_comments(cls):
-        handle = CommentsHandler()
-        qn_comments = handle.get_qn_comments()
-        handle = CommentsHandler()
-        ans_comments = handle.get_ans_comments()
-        pprint(ans_comments)
-        commentsList = {
-            "question_comments":[],
-            "answer_comments":[]
-        }
-        # question comment table cols: id, comment, qn_id, user_id, create_date
-        # answer comment table cols: id, comment, ans_id, user_id, create_date
-        # user_id, comment, parent, parent_id
-        if qn_comments:
-            for comment in qn_comments:
-                qn = Comment(comment["user_id"], comment["comment"], "question", comment["qn_id"])
-                commentsList["question_comments"].append(qn.json())
-        if ans_comments:
-            for comment in ans_comments:
-                qn = Comment(comment["user_id"], comment["comment"], "answer", comment["ans_id"])
-                commentsList["answer_comments"].append(qn.json())
-
-            return commentsList
-        # from .answer import Answer
-        # for qn in cls.questions:
-        #     qn['comments'].extend(Answer.get_comments_by_qn_id(qn['id']))
-        return commentsList
+        return cls.comments
 
     @classmethod
     def get_comments_by_parent_id(cls,parent, parent_id):
-        # get_qn_comments_by_qn_id(self, qn_id):
-        # get_ans_comments_by_ans_id(self, ans_id):
-        if parent.lower() == "question":
-            handle = CommentsHandler()
-            qn_comments = handle.get_qn_comments_by_qn_id(parent_id)
-            commentsList = []
-            if qn_comments:
-                for comment in qn_comments:
-                    qn = Comment(comment["user_id"], comment["comment"], "question", comment["qn_id"])
-                    commentsList.append(qn.json())
-                return commentsList
-            return commentsList
-        elif parent.lower() == "answer":
-            handle = CommentsHandler()
-            ans_comments = handle.get_ans_comments_by_ans_id(parent_id)
-            commentsList = []
-            if ans_comments:
-                for comment in ans_comments:
-                    qn = Comment(comment["user_id"], comment["comment"], "answer", comment["ans_id"])
-                    commentsList.append(qn.json())
-                return commentsList
-            return commentsList
-        
-        return None
+        return list(filter(lambda comm: comm['parent'] == parent and comm['parent_id'] == parent_id, cls.comments))
 
     @classmethod
-    def check_for_repeated_comment(cls, comment, comment_type):
+    def get_no_of_comments(cls):
+        return len(cls.comments)
+
+    @classmethod
+    def check_for_repeated_comment(cls, body, parent, parent_id):
         ''' check if an comment has already been given '''
-        # check_repeated_ans_comment(self, comment):
-        # check_repeated_qn_comment(self, comment):
-        if comment_type.lower() == "question":
-            handle = CommentsHandler()
-            qn = handle.check_repeated_qn_comment(comment)
-            if qn:
-                return True;
-            return False
-        elif comment_type.lower() == "answer":
-            handle = CommentsHandler()
-            ans = handle.check_repeated_ans_comment(comment)
-            if ans:
-                return True;
-            return False
-        
+        for comm in cls.comments:
+            if comm['parent'].lower() == parent.lower():
+                if str(comm['parent_id']) == str(parent_id):
+                    if comm['body'].lower() == body.lower():
+                        return True
         return False
--- API/resources/question.py
+++ API/resources/question.py
@@ -1,76 +1,15 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_identity
-
 from API.models.question import Question
-from .utilities import clean_input, check_description_length, check_title_length
+from .utilities import clean_input, check_body_length, check_title_length
 
 class Questions(Resource):
     
     def get(self, questionId):
         qn = Question.get_question_by_id(questionId)
         if qn:
-            return { 'question': qn.json() }, 200
+            return { 'question': qn }, 200
         return { 'message': 'Question not found' }, 404
-        
-    @jwt_required
-    def delete(self, questionId):
-        ''' this method deletes a question only if the user is the author '''
-        user_id = get_jwt_identity()
-
-        question = Question.get_question_by_id(questionId)
-        if question:
-            if user_id == question.user_id:
-                try:
-                    if Question.delete_question(question.qn_id):
-                        return {"message":"Question was successfully deleted"}, 200
-                    return {"message": "Question was not deleted, Please try again later..."}, 500
-                except:
-                    return {'message': 'There was a problem adding the question'}, 500
-            return {'message': "Sorry you don't have permission to delete this question"}, 401
-
-        return {'message': 'Question not found'}, 404
-
-    @jwt_required
-    def put(self, questionId):
-        ''' this method deletes a question only if the user is the author '''
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'body',
-            type=str,
-            required=True,
-            help="The body field can't be empty"
-        )
-        data = parser.parse_args()
-        
-        user_id = get_jwt_identity()
-
-        db_question = Question.get_question_by_id(questionId)
-        if db_question:
-            if db_question.user_id == user_id:
-                ''' validate data sent '''
-                body = data['body'].strip().lower()
-                if not clean_input(body):
-                    return {'message': 'The body should be a string'}, 400
-
-                if not check_description_length(body):
-                    return {'message': 'The question description is too short'}, 400
-
-                ''' validate that the question hasn't been asked before '''
-
-                if Question.check_qn_description(body):
-                    return {'message': 'Sorry, that question with already exits'}, 400
-
-                try:
-                    if Question.update_question(db_question.qn_id, body):
-                        return {"message": "Question was successfully updated"}, 200
-                        
-                    return {"message": "Question was not updated, Please try again later..."}, 500
-                except:
-                    return {'message': 'There was a problem adding the question'}, 500
-            return {'message': "Sorry you don't have permission to update this question"}, 401
-
-        return {'message': 'Question not found'}, 404
 
 
 class QuestionList(Resource):
@@ -79,7 +18,6 @@ class QuestionList(Resource):
         ''' this method gets all the questions on the platform '''
         return {'questions': Question.get_questions()}, 200
         
-    @jwt_required
     def post(self):
         ''' this method add a question '''
         parser = reqparse.RequestParser()
@@ -97,33 +35,30 @@ class QuestionList(Resource):
         )
         data = parser.parse_args()
         ''' validate data sent '''
-        body = data['body'].strip().lower()
-        title = data['title'].strip().lower()
-        if not clean_input(title):
+        if not clean_input(data['title']):
             return {'message': 'The title should be a string'}, 400
         
-        if not check_title_length(title):
+        if not check_title_length(data['title']):
             return {'message': 'The title should be at least 8 characters'}, 400
 
-        if not clean_input(body):
+        if not clean_input(data['body']):
             return {'message': 'The body should be a string'}, 400
             
-        if not check_description_length(body):
-            return {'message': 'The question description is too short.'}, 400
+        if not check_body_length(data['body']):
+            return {'message': 'The body should be atleast 15 characters'}, 400
             
         ''' validate that the question hasn't been asked before '''
-        if Question.check_qn_title(title):
+        if Question.check_qn_title(data['title']):
             return {'message': 'Sorry, a question with that title has already been asked'}, 400
 
-        if Question.check_qn_description(body):
+        if Question.check_qn_body(data['body']):
             return {'message': 'Sorry, a question with that body has already been asked'}, 400
-        
-        user_id = get_jwt_identity()
-        # print(user_id, "userid")
-        question = Question(title, body, user_id)
+        # setting ids
+        qn_id = Question.get_no_of_qns() + 1
+        question = Question(qn_id, data['title'], data['body'])
 
         try:
-            question.add_question(question)
+            Question.add_question(question)
         except:
             return {'message': 'There was a problem adding the question'}, 500
 
