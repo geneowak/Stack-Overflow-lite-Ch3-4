3f3d02ce526a5ba6d101b5e921a2ec43962d237f
Eugene Owak <13153526+geneowak@users.noreply.github.com>
2019-01-23 11:47:50 +0300

    Develop (#7)
    * Merge all changes done so far... (#3)
    
    * add some database initialization files
    
    * connect to postgres db and add some database handling files with some code
    
    * add the register User resource and adding code in the respective db handlers
    
    * add questions and answers endpoints. Solved bugs with connecting and adding data to the db. Added login and signup endpoints
    
    * add update answer and accept answer endpoints. Move to write tests for the endpoints...
    
    * finish required endpoints with their tests. Added configuration for travis and coveralls
    
    * correct error in travis file i.e. remove db commands from install into before_script
    
    * correct error db commands i.e. change from '- U' to '-U'
    
    * edit travis before script commands
    
    * edit travis file
    
    * Update .travis.yml
    
    * edit db file
    
    * Update README.md
    
    * added heroku congirations
    
    * Update db_handler.py
    
    * Worked on all the feedback from LFAs. Added tests for user.
    
    * update tests to reflect changes made in code
    
    * Update README.md
    
    * Add some functionality for get user questions and get user answers. Edit README to reflect the implemented endpoints
    
    * Update README.md
    
    * edit readme file and index.html to reflect endpoints
    
    * Add some functionality for get user questions and get user answers. Edit README to reflect the implemented endpoints
    
    * edit readme file and index.html to reflect endpoints
    
    * add doc strings in app.py file
    
    * Update README.md
    
    * Add comments end points (#5)
    
    * refactor database table columns to be more descriptive
    
    * Refactor database handling classes and document them. 
    
    * document model classes
    
    * add check to create database if it doesn't exist before the first request
    
    * Add post comment to question and post comment to answer
    
    * Add get Comments endpoint
    
    * Add get comments by question Id and get comments by answer id
    
    * Add developed endpoints to README
    
    * Correct typo in README

---
+++ API/database/comment_db_handler.py
@@ -0,0 +1,188 @@
+from .db_handler import DbHandler
+from pprint import pprint
+import datetime
+from .database_ini import table_names
+
+
+class CommentsHandler(DbHandler):
+    '''
+    This method handles all the database functions of the comment model
+    '''
+
+    # question comment table cols: id, comment, qn_id, user_id, create_date
+    # answer comment table cols: id, comment, ans_id, user_id, create_date
+    def __init__(self):
+        super().__init__()
+        self.qn_table_name = table_names["question_comments"]
+        self.ans_table_name = table_names["answer_comments"]
+
+    def insert_qn_comment(self, user_id, qn_id, comment):
+        ''' adds a question comment to the database '''
+        try:
+            query = "INSERT INTO " + self.qn_table_name + " (comment, qn_id, user_id, create_date) VALUES(%s,%s,%s,%s)"
+            self.cursor.execute(
+                query, (comment, qn_id, user_id, datetime.datetime.now()))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def update_qn_comment(self, comment, qn_id):
+        ''' updates a question comment in the database '''
+        try:
+            query = "UPDATE "+self.qn_table_name + " SET comment=%s WHERE qn_id=%s"
+            self.cursor.execute(query, (comment, qn_id))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def get_qn_comments_by_qn_id(self, qn_id):
+        ''' returns the specified question comment from the database '''
+        try:
+            query = "SELECT comment, id, user_id, qn_id FROM "+self.qn_table_name+" WHERE qn_id=%s"
+            self.cursor.execute(query, (qn_id,))
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def get_qn_comments(self):
+        try:
+            query = "SELECT comment, id, user_id, qn_id FROM "+self.qn_table_name+""
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint(error)
+            super().close_conn()
+            return None
+
+    def delete_qn_comment_by_qn_id(self, comment_id):
+        ''' deletes the specified question comment from the database '''
+        try:
+            query = "DELETE FROM "+self.qn_table_name+" WHERE id=%s"
+            self.cursor.execute(query, (comment_id,))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def check_repeated_qn_comment(self, comment):
+        ''' checks if the specified question comment already exists in the database '''
+        try:
+            query = "SELECT comment, id, user_id, qn_id FROM " + \
+                self.qn_table_name+" WHERE comment=%s"
+            self.cursor.execute(query, (comment,))
+            row = self.cursor.fetchone()
+            super().close_conn()
+            return row
+        except (Exception) as error:
+            pprint("and error occured", error)
+            super().close_conn()
+            raise error
+
+    def insert_ans_comment(self, user_id, ans_id, comment):
+        ''' adds an answer comment to the database '''
+        try:
+            query = "INSERT INTO "+self.ans_table_name + \
+                " (comment, ans_id, user_id, create_date) VALUES(%s,%s,%s,%s)"
+            self.cursor.execute(
+                query, (comment, ans_id, user_id, datetime.datetime.now()))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def update_ans_comment(self, user_id, ans_id, comment):
+        ''' updates an answer comment in the database '''
+        try:
+            query = "UPDATE "+self.ans_table_name + " SET comment=%s WHERE ans_id=%s"
+            self.cursor.execute(query, (comment, ans_id))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def get_ans_comments_by_ans_id(self, ans_id):
+        ''' returns the specified answer comment from the database '''
+        try:
+            query = "SELECT comment, id, user_id, ans_id FROM "+self.ans_table_name+" WHERE ans_id=%s"
+            self.cursor.execute(query, (ans_id,))
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def get_ans_comments(self):
+        try:
+            query = "SELECT comment, id, user_id, ans_id FROM "+self.ans_table_name+""
+            self.cursor.execute(query)
+            rows = self.cursor.fetchall()
+            super().close_conn()
+            return rows
+        except (Exception) as error:
+            pprint("an error occured...")
+            pprint(error)
+            super().close_conn()
+            return None
+
+    def delete_ans_comment_by_id(self, comment_id):
+        ''' deletes the specified answer comment from the database '''
+        try:
+            query = "DELETE FROM "+self.ans_table_name+" WHERE id=%s"
+            self.cursor.execute(query, (comment_id,))
+            # close connection
+            super().close_conn()
+            return True
+        except (Exception) as error:
+            pprint("and error occured", error)
+            self.conn.rollback()
+            super().close_conn()
+            raise error
+
+    def check_repeated_ans_comment(self, comment):
+        ''' checks if the specified answer comment already exists in the database '''
+        try:
+            query = "SELECT comment, id, user_id, ans_id FROM " + \
+                self.ans_table_name+" WHERE comment=%s"
+            self.cursor.execute(query, (comment,))
+            row = self.cursor.fetchone()
+            super().close_conn()
+            return row
+        except (Exception) as error:
+            pprint("and error occured", error)
+            super().close_conn()
+            raise error
+
+    
+        
---
+++ API/models/user.py
@@ -0,0 +1,43 @@
+from API.database.user_db_handler import UserHandler
+
+
+class UserModel:
+    ''' user_id, username, password, create_date, last_login '''
+
+    def __init__(self, username, password, user_id=None, email=None):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.user_id = user_id
+
+    def add_user(self):
+        handle = UserHandler()
+        if handle.insert_user(self.username, self.password, self.email):
+            return True
+        return False
+
+    @staticmethod
+    def get_user_by_username(username):
+        handle = UserHandler()
+        row = handle.get_user_by_username(username)
+        if row:
+            return UserModel(row['username'], row['password'], row['user_id'], row['email'])
+        return None
+
+    @staticmethod
+    def get_user_by_email(email):
+        handle = UserHandler()
+        row = handle.get_user_by_email(email)
+        # print(row)
+        if row:
+            return UserModel(row['username'], row['password'], row['user_id'], row['email'])
+        return None
+    
+    @staticmethod
+    def get_user_by_id(user_id):
+        handle = UserHandler()
+        row = handle.get_user_by_id(user_id)
+        if row:
+            return UserModel(row['username'], row['password'], row['user_id'], row['email'])
+        return None
+
